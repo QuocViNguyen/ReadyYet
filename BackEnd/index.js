199f95d3b5bb044aa2087da07b55a8ca1e4256ba
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-08-03 21:28:51 -0500

    A mess, passport having some problem with authentication function

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,13 +3,52 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require('./models/users');
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const initializePassport = require('./passport-config');
+const flash = require("express-flash");
+const session = require("express-session");
+
 //#endregion
 
 //#region Properties
 const dbURI = "mongodb+srv://quocvinguyen:zsqRZXyq2n0yA5ki@cluster-basic.levl5.mongodb.net/ReadyYetDB?retryWrites=true&w=majority";
 const app = express();
 const PORT = 4000;
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+
+initializePassport(
+  passport,
+  email => {
+    User.findOne({'email' : email}, function( error, docs){
+        if (error){
+            return null;
+        }else{
+            return docs;
+        }
+    })
+  },
+  id =>{
+    User.findOne({'id' : id}, function( error, docs){
+        if (error){
+            return null;
+        }else{
+            return docs;
+        }
+    })
+    return null;
+  }
+)
+
+
+app.use(session({
+    secret: 'jarvis IV',
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+
 //#endregion
 
 //#region DB connection
@@ -49,22 +88,31 @@ app.get("/delete-all", (req, res)=>{
     })
 })
 
-app.get("/login", async (req, res)=>{
-    try {
-        // const harshedPwd = await bcrypt.hash(req.body.password, 10);
-        User.findOne({'email' : req.query.email}, function( error, docs){
-            if (error){
-                console.log("Error" + error);
-                res.send(error);
-            }else{
-                console.log("Result: "+ docs);
-                res.send("RESULT: "+ docs);
-            }
-        })
+app.post("/login", passport.authenticate('local', {
+    failureFlash: true
+}), (req, res)=>{
+    console.log(req)
+    // try {
+    //     // const harshedPwd = await bcrypt.hash(req.body.password, 10);
+    //     console.log(User.findOne({'email' : req.query.email}))
+    //     User.findOne({'email' : req.query.email}, function( error, docs){
+    //         if (error){
+    //             console.log("Error" + error);
+    //             res.send(error);
+    //         }else{
+    //             console.log("POST Result: "+ docs.firstname);
+    //             res.send("RESULT: "+ docs);
+    //         }
+    //     })
         
-    } catch {
+    // } catch {
         
-    }
+    // }
+    res.send("LOGIN SUCCESSFUL");
+})
+
+app.post("/posted", (req, res) =>{
+    res.send("POSTED");
 })
 
 
