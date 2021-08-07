ca45de5d7924e50e8d6d05b1deb53e4480dd19dd
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-08-07 14:16:17 -0500

    View and format fully orders

--- FrontEnd/ready-yet-app/src/Pages/PharmacistPage/ViewOrderPage.js
+++ FrontEnd/ready-yet-app/src/Pages/PharmacistPage/ViewOrderPage.js
@@ -3,16 +3,40 @@ import { DataGrid } from '@material-ui/data-grid';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import { Box, Button } from '@material-ui/core';
 
+function formatDate(isoTime) {
+  const nth = function(d) {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
+  const parseTime = new Date(isoTime)
+  let themonth = parseTime.toLocaleString('default', { month: 'short' });
+  let thedate = parseTime.getDate()  + nth(parseTime.getDate());
+  let theyear = parseTime.getFullYear();
+  let thetime = `${parseTime.getHours()}:${parseTime.getMinutes()}`
+  let result = `${themonth} ${thedate} ${theyear} at ${thetime}`
+  return result;
+}
 
 function mapMyOrder(order) {
+    const time = formatDate(order.pickuptime)
+    
     const rowItem = {
         id: order.patient[0]._id,
         lastName: order.patient[0].lastname,
         firstName: order.patient[0].firstname,
         email: order.patient[0].email,
-        phoneNumber: order.patient[0].phonenumber
+        phoneNumber: order.patient[0].phonenumber,
+        pickUpTime: time
     }
+      
     return rowItem;
 }
 
@@ -38,7 +62,7 @@ function ViewOrderPage() {
           field: 'email',
           headerName: 'Email',
           type: 'string',
-          width: 120,
+          width: 260,
           editable: false,
           sortable: true,
         },
@@ -48,6 +72,14 @@ function ViewOrderPage() {
           description: 'Patients phone number',
           sortable: true,
           width: 180,
+        },
+        {
+          field: 'pickUpTime',
+          headerName: 'Pick Up Time',
+          description: 'Estimate pick up time',
+          type: 'string',
+          sortable: true,
+          width: 200,
         }
       ];
 
@@ -56,21 +88,26 @@ function ViewOrderPage() {
             const data = response.data;
             const mapped = data.map(mapMyOrder);
             setRow(mapped);
+            console.log(rows);
         })
-    })
+    }, [])
 
-
-    return ( 
-        <div style={{ height: 400, width: '60%' }}>
-        <CircularProgress/>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            checkboxSelection
-            // disableSelectionOnClick
-          />
-        </div>
+    return (
+    <Box className='h-screen w-screen overflow-x-hidden overflow-y-scroll'>
+        <Box className='h-1/6 md:h-1/6 flex items-center justify-center'>ORDERS</Box>
+        <Box className='h-3/6 md:h-2/6 flex items-center justify-center'>
+        
+          <Box className='w-8/12'>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                checkboxSelection
+                autoHeight={true}
+              />
+          </Box>
+        </Box>
+    </Box>
      );
 }
  
